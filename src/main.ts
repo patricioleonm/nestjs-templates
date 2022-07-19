0ac5ec6154332465ba5217ab28ec1cce7bfe5f0d
Patricio León M <patricioleonm@gmail.com>
2022-07-19 18:59:04 -0400

    feat: base nestjs api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import helmet from 'helmet';
+
+const LISTEN_PORT = process.env.LISTEN_PORT ?? 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  app.use(helmet);
+
+  app.enableCors({});
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      skipMissingProperties: true,
+    }),
+  );
+
+  await app.listen(LISTEN_PORT).then(async () => {
+    const url = await app.getUrl();
+    Logger.log(`Listening on ${url}`, 'Main');
+  });
 }
 bootstrap();
